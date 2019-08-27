09eacfe0d0532471dd58b7297f31e914489fba6c
Kay Jay <kharris2k16@gmail.com>
2019-08-27 15:22:43 -0700

    completed session configuration

--- server.js
+++ server.js
@@ -2,14 +2,11 @@
 const express = require("express");
 const server = express();
 server.use(express.json());
-
 // use helmet
 const helmet = require('helmet')
 server.use(helmet());
 //use my middleware
 server.use(logger);
-//use my routes
-
 //use my helpers
 const db = require("./userModel");
 
@@ -21,21 +18,34 @@ server.get("/", (req, res) => {
 });
 
 //the actual code for the project
+//declare sessionConfig
+const sessionConfig = {
+  name: 'Shieda', //defaults to sid, provide name for security
+  secret: 'Rhaast', // ?
+  cookie: {
+    maxAge: 1000 * 60, //set how long cookie is valid for in ms
+    secure: false, //true in production
+    httpOnly: true, //always set this to true, denies js access to cookie
+  },
+  resave: false, //do we want to recreate a session if the session hasnt changed
+  saveUninitialized: false, //ALWAYS FALSE BY DEFAULT: GDPR Laws against setting cookies auotmatically, require user consent
+};
+
+
+//use session
+const session = require('express-session');
+server.use(session(sessionConfig));
+
 //use bcrypt
 const bcrypt = require('bcryptjs');
 
-//--
 // register new user
 server.post("/api/register", async (req, res) => {
   const userInfo = req.body;
-  // console.log(userInfo);
-
   //generate the hash
-  const hash = bcrypt.hashSync(userInfo.password, 2);
-
+  const hash = bcrypt.hashSync(userInfo.password, 12);
   //set the userpassword to our new hashed value
   userInfo.password = hash;
-  console.log(hash)
   
   try {
     if(userInfo){
@@ -54,32 +64,6 @@ server.post("/api/register", async (req, res) => {
       message: "Error"
     });
 }})
-//--
-// server.post("/api/register", async (req, res) => {
-//   const userInfo = req.body;
-//   // console.log(userInfo);
-//   const hash = bcrypt.hashSync(userInfo.password, 13);
-
-//   userInfo.password = hash;
-//   console.log(userInfo);
-//   try {
-//     if(userInfo){
-//       const newUser = await db.addUser(userInfo);
-
-//       if(newUser){
-//         res.status(201).json(newUser)
-//       } else {
-//         res.status(400).json({
-//           message: "Error Adding the User to the database"
-//         });
-//       }
-//     }
-//   } catch (err) {
-//     res.status(500).json({
-//       message: "Error"
-//     });
-// }})
-//--
 
 //authenticate and log in existing user
 server.post("/api/login", (req, res) => {
@@ -131,7 +115,7 @@ function logger(req, res, next) {
 function verify(req, res, next){
   //get user information from headers
   const { username,  password} = req.headers
-  //use findby method in model to username from req.body
+  //use findby method in model to get username from req.body
   if (username && password) {
     db.findBy({ username })
     .first()
